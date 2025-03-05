d696a370865ab48bc3dad9b01ab31a05165b73d2
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2025-03-05 10:38:11 +0100

    add locals typing

--- packages/starlight-utils/virtual.d.ts
+++ packages/starlight-utils/virtual.d.ts
@@ -2,3 +2,9 @@ declare module "virtual:starlight-utils/config" {
   const Config: import("./config").StarlightUtilsConfig;
   export default Config;
 }
+
+declare namespace App {
+  type StarlightLocals = import("@astrojs/starlight").StarlightLocals;
+  // Define the `locals.t` object in the context of a plugin.
+  interface Locals extends StarlightLocals {}
+}
